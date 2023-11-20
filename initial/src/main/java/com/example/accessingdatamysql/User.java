459dd546b4a016972b2c7007599959a55adbd85f
lhl <2425731780@qq.com>
2023-11-21 00:14:03 +0800

    name-describe

---
+++ initial/src/main/java/com/example/accessingdatamysql/User.java
@@ -0,0 +1,41 @@
+package com.example.accessingdatamysql;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Integer id;
+
+  private String name;
+
+  private String Describe;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescribe() {
+    return Describe;
+  }
+
+  public void setDescribe(String Describe) {
+    this.Describe = Describe;
+  }
+}
